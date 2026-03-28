a1b39496511deacaf9cdebc03ae8b2573f3369aa
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-03-27 21:11:10 -0400

    Ejercicio 4

---
+++ Ejercicio4/Ejercicio4/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+class Ejercicio4
+{
+    static void Main()
+    {
+        Queue<int> cola = new Queue<int>();
+        cola.Enqueue(100);
+        cola.Enqueue(200);
+        cola.Enqueue(300);
+        cola.Enqueue(400);
+        cola.Enqueue(500);
+
+        Console.Write("Contenido de la cola: ");
+        foreach (int n in cola)
+        {
+            Console.Write(n+ ", ");
+        }
+    }
+}
