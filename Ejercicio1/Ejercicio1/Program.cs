f8333cb66b4c2ff7889549419af935dfdd817b45
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-03-27 20:34:22 -0400

    Ejercicio 1

---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Ejercicio1
+{
+    static void Main()
+    {
+        Stack<int> pila = new Stack<int>();
+        pila.Push(10);
+        pila.Push(20);
+        pila.Push(30);
+        pila.Push(40);
+
+        Console.WriteLine("Contenido de la pila (del último al primero):");
+        foreach (int numero in pila)
+        {
+            Console.WriteLine(numero);
+        }
+    }
+}
