72d39cb7463307ee07f0e11337a835f65770a120
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-03-27 20:48:02 -0400

    Ejercicio2

---
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Ejercicio2
+{
+    static void Main()
+    {
+        Stack<int> pila = new Stack<int>(new[] { 1, 2, 3, 4, 5 });
+
+        VaciarYMostrar(pila);
+    }
+
+    static void VaciarYMostrar(Stack<int> pila)
+    {
+        Console.WriteLine("Eliminando elementos...");
+        while (pila.Count > 0)
+        {
+            pila.Pop();
+        }
+        Console.WriteLine("La pila se ha vaciado. Elementos actuales: " + pila.Count);
+    }
+}
