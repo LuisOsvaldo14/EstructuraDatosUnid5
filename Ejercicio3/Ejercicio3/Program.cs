cade24d9ffc30785d0671cd28e7461a80a1e5b92
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-03-27 21:00:30 -0400

    Ejercicio3

---
+++ Ejercicio3/Ejercicio3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Ejercicio2
+{
+    static void Main()
+    {
+        Stack<string> pila = new Stack<string>(new[] { "Jose", "Luis", "Ismael" });
+
+        VaciarYMostrar(pila);
+    }
+
+    static void VaciarYMostrar(Stack<string> p)
+    {
+        Console.WriteLine("Eliminando elementos...");
+        while (p.Count > 1)
+        {
+            p.Pop(); 
+        }
+        Console.WriteLine("Elementos actuales: ");
+        foreach (string s in p) Console.WriteLine(s);
+    }
+}
