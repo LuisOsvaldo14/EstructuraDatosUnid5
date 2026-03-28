bf35b6e0acadbed5e5116a82599e9aed967436cb
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-03-27 21:23:17 -0400

    Ejercicio 5

---
+++ Ejercicio 5/Ejercicio 5/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+class Ejercicio5
+{
+    static void Main()
+    {
+        Queue<int> cola = new Queue<int>();
+        cola.Enqueue(100);
+        cola.Enqueue(200);
+        cola.Enqueue(300);
+        cola.Enqueue(400);
+        cola.Enqueue(500);
+        int Acumular = 0;
+        Console.Write("La suma de los valores ");
+        foreach (int n in cola)
+        {
+            Acumular = n + Acumular;
+            Console.Write(n + "," );
+        }
+        Console.WriteLine($"es igual a {Acumular}");
+
+    }
+}
